a225b87e8158d83bcea2b90afa2dbe63007032b6
jpaltridge1 <59751767+jpaltridge1@users.noreply.github.com>
2020-01-29 17:29:38 -0700

    update class

---
+++ CorePortfolio02-JeffPaltridge/CorePortfolio02-JeffPaltridge/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorePortfolio02_JeffPaltridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputWeight;
+            double totalWeight;
+            const double price1 = 0.55;
+
+
+        }
+    }
+}
