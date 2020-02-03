32a7abf09005df9596b9d3c7d8be7c4ab02c7285
jpaltridge1 <59751767+jpaltridge1@users.noreply.github.com>
2020-02-03 16:45:39 -0700

    update

--- CorePortfolio02-JeffPaltridge/CorePortfolio02-JeffPaltridge/Program.cs
+++ CorePortfolio02-JeffPaltridge/CorePortfolio02-JeffPaltridge/Program.cs
@@ -10,9 +10,114 @@ namespace CorePortfolio02_JeffPaltridge
     {
         static void Main(string[] args)
         {
-            string inputWeight;
-            double totalWeight;
-            const double price1 = 0.55;
+
+            /*  Purpose: Calulate the cost of the gravel with price determined by weight and add delivery fee if delivery required
+             *  Input: total weight of gravel and if the customer wants it delivered or not
+             *  Process(es): if/else , switch, readline, writeline
+             *  Output: example:
+             *
+             *               Welcome to Stoney Gravel Pit! 
+             *
+             *              Please enter the weight of gravel required: 2500 
+             *              Do you require delivery (Y/N)? y 
+             *
+             *              The charge for 2500 lb of gravel is $0.35 per lb. 
+             *
+             *              Subtotal:       $875.00 
+             *              Delivery:        $26.25
+             *              GST:             $45.06
+             *              -----------------------
+             *              Total:          $946.31 
+             *
+             *              Thank you for your purchase! We hope your day rocks. 
+             *              
+             *              
+             * Author: Jeff Paltridge
+             * Last modified: 2020.02.03 
+             */
+
+            string inputWeight, deliveryInput;
+            double totalWeight, totalGst, totalCost, deliveryCost, gravelCost, gravelPrice;
+            
+            const double PRICE1 = 0.55;
+            const double PRICE2 = 0.45;
+            const double PRICE3 = 0.35;
+            const double PRICE4 = 0.25;
+            const double PRICE5 = 0.15;
+            const double PRICE6 = 0.10;
+            const double GST = 0.05;
+            const double WEIGHT1 = 1000;
+            const double WEIGHT2 = 2000;
+            const double WEIGHT3 = 3000;
+            const double WEIGHT4 = 4000;
+            const double WEIGHT5 = 5000;
+            const double DELIVERYFEE = 0.03;
+            const double DELIVERYDEFAULT = 0.00;
+            Console.WriteLine("Welcome to stoney Gravel Pit!");
+            Console.WriteLine();
+            
+            Console.Write("Please enter the weight of gravel required: ");
+            inputWeight = Console.ReadLine();
+            totalWeight = double.Parse(inputWeight);
+
+            if (totalWeight < WEIGHT1)
+            {
+                gravelPrice = PRICE1;
+            }
+
+                else if (totalWeight >= WEIGHT1 && totalWeight < WEIGHT2)
+                {
+                    gravelPrice = PRICE2;
+                }
+
+                else if (totalWeight >= WEIGHT2 && totalWeight < WEIGHT3)
+                {
+                    gravelPrice = PRICE3;
+                }
+                else if (totalWeight >= WEIGHT3 && totalWeight < WEIGHT4)
+                {
+                    gravelPrice = PRICE4;
+                }
+
+                else if (totalWeight >= WEIGHT4 && totalWeight < WEIGHT5)
+                {
+                    gravelPrice = PRICE5;
+                }
+
+            else
+            {
+                gravelPrice = PRICE6;            
+            }
+
+            Console.Write("Do you require delivery (Y/N)? ");
+            deliveryInput = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine($"The charge for {totalWeight:0} lb of gravel is ${gravelPrice:0.00} per lb.");
+
+            switch (deliveryInput)
+            {
+
+
+                case (deliveryInput.ToUpper)"Y":
+                    {
+                        deliveryCost = gravelPrice * DELIVERYFEE;
+                    }
+                
+                case (deliveryInput.ToUpper) != "Y"
+                    {
+                        if (totalWeight <= 4800)
+                        { 
+                            deliveryCost = DELIVERYDEFAULT; 
+                            
+                        }
+                    }
+
+            }//eos
+
+
+           
+            Console.WriteLine();
+
 
 
         }
